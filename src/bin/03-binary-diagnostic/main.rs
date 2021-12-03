0380af357ee4783f5d14502e78bf70031ef2be29
Roland-Georg Müller <roland.mueller@consileo.net>
2021-12-03 13:35:40 +0100

    Add solution for day 3 part two - not yet correct

--- src/bin/03-binary-diagnostic/main.rs
+++ src/bin/03-binary-diagnostic/main.rs
@@ -1,5 +1,6 @@
 use std::fs;
 use bitvec::prelude::*;
+use std::io::Lines;
 
 const COLUMNS: usize = 12;
 
@@ -31,13 +32,78 @@ fn main() {
         },
     };
 
-    let columns = init_columns(input_string);
-    let (gamma, epsilon) = calc_gamma_and_epsilon_bits(columns);
+    let columns = init_columns(input_string.clone());
+    let (gamma, epsilon) = calc_gamma_and_epsilon(columns);
 
+    for i in 0..COLUMNS {
+        let col = columns[i];
+        let primary_char = if col.ones >= col.zeros { "1" } else { "0" };
+        let secondary_char = if col.zeros <= col.ones { "0" } else { "1" };
+        println!("Column {}s\tprimary value is {}\t secondary value is {}", i, primary_char, secondary_char);
+    }
     println!("Submarine consumes {} power. Gamma rate is {}. Epsilon rate is {}", gamma * epsilon, gamma, epsilon);
+    let (oxygen, co2) = calc_oxygen_and_co2(input_string.clone(), columns);
+
+    // initially 1097333, 3797, 289
+    // later 5815160, 2743, 2120
+    println!("Submarine's life support rate is {}. Oxygen generator rating is {}. CO2 scrubber rating is {}", oxygen*co2, oxygen, co2);
 }
 
-fn calc_gamma_and_epsilon_bits(columns: [Column; COLUMNS]) -> (u32, u32) {
+fn calc_oxygen_and_co2(input: String, columns: [Column; COLUMNS]) -> (u32, u32) {
+    // The calculation is still incorrect...
+    let mut lines_oxygen: Vec<&str> = input.lines().collect();
+    let mut lines_co2: Vec<&str> = lines_oxygen.clone();
+
+    for i in 0..COLUMNS {
+        if lines_oxygen.len() == 1 {
+            println!("aborting oxygen filtering after {}", i);
+            break;
+        }
+        let col = columns[i];
+        let primary_char = if col.ones >= col.zeros { '1' } else { '0' };
+
+        lines_oxygen = lines_oxygen.iter().filter(|line| {
+            match line.chars().nth(i) {
+                Some(ch) => ch == primary_char,
+                None => false
+            }
+        })
+            .map(|x| *x)
+            .collect();
+    }
+
+    for i in 0..COLUMNS {
+        if lines_co2.len() == 1 {
+            println!("aborting co2 filtering after {}", i);
+            break;
+        }
+        let col = columns[i];
+        let secondary_char = if col.zeros <= col.ones { '0' } else { '1' };
+
+        lines_co2 = lines_co2.iter().filter(|line| {
+            match line.chars().nth(i) {
+                Some(ch) => ch == secondary_char,
+                None => false
+            }
+        })
+            .map(|x| *x)
+            .collect();
+    }
+    println!("oxygen lines: {:?}", lines_oxygen);
+    println!("co2 lines: {:?}", lines_co2);
+
+    let mut oxygen_bits: BitVec<Msb0> = BitVec::new();
+    let mut co2_bits: BitVec<Msb0> = BitVec::new();
+    lines_oxygen.first().unwrap().chars().map(|ch| ch == '1').for_each(|b| oxygen_bits.push(b));
+    lines_co2.first().unwrap().chars().map(|ch| ch == '1').for_each(|b| co2_bits.push(b));
+    let oxygen = oxygen_bits.load::<u32>();
+    let co2 = co2_bits.load::<u32>();
+
+
+    return (oxygen, co2)
+}
+
+fn calc_gamma_and_epsilon(columns: [Column; COLUMNS]) -> (u32, u32) {
     let mut gamma_bits: BitVec<Msb0> = BitVec::new();
     let mut epsilon_bits: BitVec<Msb0> = BitVec::new();
 
@@ -54,8 +120,8 @@ fn calc_gamma_and_epsilon_bits(columns: [Column; COLUMNS]) -> (u32, u32) {
             epsilon_bits.push(secondary);
         });
 
-    let gamma: u32 = gamma_bits.load_be::<u32>();
-    let epsilon: u32 = epsilon_bits.load_be::<u32>();
+    let gamma: u32 = gamma_bits.load::<u32>();
+    let epsilon: u32 = epsilon_bits.load::<u32>();
 
     return (gamma, epsilon);
 }
