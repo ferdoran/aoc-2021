09e62e660597f23cb1ec3f54bbc7d59580ca6dc3
Roland-Georg Müller <roland.mueller@consileo.net>
2021-12-06 17:00:50 +0100

    add solution for day 6 part 2 - had to rework previous solution

--- src/bin/06-lanternfish/main.rs
+++ src/bin/06-lanternfish/main.rs
@@ -1,72 +1,25 @@
-use std::borrow::BorrowMut;
-
-const DEFAULT_TIMER: isize = 6;
-const DEFAULT_TIMER_NEW_FISH: isize = DEFAULT_TIMER + 2;
-const DAYS_TO_SIMULATE: isize = 80;
-
-struct Fish {
-    timer: isize
-}
-
-impl Copy for Fish {}
-
-impl Clone for Fish {
-    fn clone(&self) -> Self {
-        return Fish {
-            timer: self.timer
-        }
-    }
-}
-
-impl Fish {
-    fn decrement(&mut self) -> bool {
-        self.timer -= 1;
-        if self.timer < 0 {
-            self.timer = DEFAULT_TIMER;
-            return true;
-        }
-
-        return false;
-    }
-}
-
-struct FishPopulation {
-    fish: Vec<Fish>
-}
-
-impl FishPopulation {
-    fn update(&mut self) {
-        let mut current_population: Vec<Fish> = self.fish.clone();
-        let mut updated_population: Vec<Fish> = Vec::new();
-        for fish in current_population.iter_mut() {
-            if fish.decrement() {
-                updated_population.push(Fish{timer: DEFAULT_TIMER_NEW_FISH});
-            }
-            updated_population.push(*fish);
-        }
-        self.fish = updated_population;
-    }
-
-    fn count(&self) -> usize {
-        return self.fish.len();
-    }
-}
+const DAYS_TO_SIMULATE: isize = 256; // Adapt for part 2
 
 fn main() {
     let input_string = include_str!("input.txt");
-    println!("initial pop: {}", input_string);
-    let initial_population: Vec<Fish> = input_string.split(",")
-        .map(|s| isize::from_str_radix(s, 10).unwrap())
-        .map(|timer| Fish{timer})
-        .collect();
+    let mut timers = [0_usize; 9];
 
-    let mut population = FishPopulation{fish: initial_population};
-    let len_initial_population = population.fish.len();
+    input_string.split(",")
+        .map(|s| usize::from_str_radix(s, 10).unwrap())
+        .for_each(|t| timers[t] += 1);
 
     for _ in 0..DAYS_TO_SIMULATE {
-        population.update();
+        let spawned_fish = timers[0]; // fish that give birth
+
+        for i in 0..timers.len()-1 {
+            timers[i] = timers[i+1]; // reduce timers
+        }
+
+        timers[6] += spawned_fish; // fish that give birth get their timers reset
+        timers[8] = spawned_fish; // add newly born fish
     }
 
-    println!("Starting with {} fish, after {} days they have grown to {} fishes", len_initial_population, DAYS_TO_SIMULATE, population.count());
-}
+    let result: usize = timers.iter().sum();
 
+    println!("after {} days there are {} fish", DAYS_TO_SIMULATE, result);
+}
