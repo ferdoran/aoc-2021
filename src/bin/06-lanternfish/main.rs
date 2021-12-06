a457b50d458bafef33e225fe3a7b70194f6ad690
Roland-Georg Müller <roland.mueller@consileo.net>
2021-12-06 08:34:05 +0100

    init day 6

--- src/bin/06-lanternfish/main.rs
+++ src/bin/06-lanternfish/main.rs
@@ -1,13 +1,6 @@
 use std::fs;
 
 fn main() {
-    let input = fs::read_to_string("./src/bin/06-lanternfish/input.txt");
-    let input_string = match input {
-        Ok(s) => s,
-        Err(e) => {
-            eprintln!("failed to read input file: {}", e);
-            return;
-        }
-    };
+    let input_string = include_str!("input.txt");
 }
 
